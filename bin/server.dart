aad3d6c942218ac2e2ca2b28e127ed1efdd6bae4
Patrick Stewart <pstewart@vieolab.com>
2023-07-24 09:37:31 -0700

    change(update): adding comments to our code

--- bin/server.dart
+++ bin/server.dart
@@ -1,14 +1,19 @@
+// Importing necessary libraries
 import 'dart:convert';
 import 'dart:io';
 import 'package:crypto/crypto.dart';
 import 'package:web_socket_channel/io.dart';
 
+// Setting up default ports for HTTP and P2P communication
 int httpPort = int.tryParse(Platform.environment['HTTP_PORT'] ?? '') ?? 3001;
 int p2pPort = int.tryParse(Platform.environment['P2P_PORT'] ?? '') ?? 6001;
+
+// List of initial peers if any
 List<String> initialPeers = Platform.environment['PEERS'] != null
     ? Platform.environment['PEERS']!.split(',')
     : [];
 
+// Class representing a block in the blockchain
 class Block {
   int index;
   String previousHash;
@@ -18,6 +23,7 @@ class Block {
 
   Block(this.index, this.previousHash, this.timestamp, this.data, this.hash);
 
+  // Function to convert a block to JSON format
   Map<String, dynamic> toJson() => {
         'index': index,
         'previousHash': previousHash,
@@ -26,6 +32,7 @@ class Block {
         'hash': hash,
       };
 
+  // Function to create a block from JSON data
   factory Block.fromJson(Map<String, dynamic> json) {
     return Block(
       json['index'],
@@ -37,6 +44,7 @@ class Block {
   }
 }
 
+// Class representing a peer in the network
 class Peer {
   final IOWebSocketChannel socket;
   final String address;
@@ -45,17 +53,22 @@ class Peer {
   Peer(this.socket, this.address, this.port);
 }
 
+// List of connected peers
 List<Peer> sockets = [];
 
+// Enum representing the types of messages that can be sent in the network
 enum MessageType { QUERY_LATEST, QUERY_ALL, RESPONSE_BLOCKCHAIN }
 
+// Function to get the genesis block
 Block getGenesisBlock() {
   return Block(0, "0", 1465154705, "my genesis block!!",
       "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
 }
 
+// The blockchain, initialized with the genesis block
 List<Block> blockchain = [getGenesisBlock()];
 
+// Function to handle GET requests
 void handleGet(HttpRequest request) {
   if (request.uri.path == '/blocks') {
     request.response
@@ -71,6 +84,7 @@ void handleGet(HttpRequest request) {
   }
 }
 
+// Function to handle POST requests
 void handlePost(HttpRequest request) async {
   if (request.uri.path == '/mineBlock') {
     var content = await utf8.decoder.bind(request).join();
@@ -92,6 +106,7 @@ void handlePost(HttpRequest request) async {
   }
 }
 
+// Function to initialize the HTTP server
 void initHttpServer() {
   HttpServer.bind(InternetAddress.anyIPv4, httpPort).then((server) {
     server.listen((HttpRequest request) {
@@ -104,12 +119,14 @@ void initHttpServer() {
   });
 }
 
+// Function to initialize a connection with a peer
 void initConnection(Peer peer) {
   initMessageHandler(peer.socket);
   initErrorHandler(peer);
   write(peer.socket, queryChainLengthMsg());
 }
 
+// Function to initialize the P2P server
 void initP2PServer() {
   HttpServer.bind(InternetAddress.anyIPv4, p2pPort).then((server) {
     server.listen((HttpRequest request) {
@@ -127,6 +144,7 @@ void initP2PServer() {
   });
 }
 
+// Function to initialize the message handler for a socket
 void initMessageHandler(IOWebSocketChannel ws) {
   ws.stream.listen((message) {
     var messageMap = jsonDecode(message);
@@ -145,17 +163,20 @@ void initMessageHandler(IOWebSocketChannel ws) {
   });
 }
 
+// Function to initialize the error handler for a peer
 void initErrorHandler(Peer peer) {
   peer.socket.sink.done.catchError((error) {
     closeConnection(peer);
   });
 }
 
+// Function to close a connection with a peer
 void closeConnection(Peer peer) {
   print('connection failed to peer: ${peer.address}:${peer.port}');
   sockets.remove(peer);
 }
 
+// Function to generate the next block in the chain
 Block generateNextBlock(String blockData) {
   Block previousBlock = getLatestBlock();
   int nextIndex = previousBlock.index + 1;
@@ -166,11 +187,13 @@ Block generateNextBlock(String blockData) {
       nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash);
 }
 
+// Function to calculate the hash for a block
 String calculateHashForBlock(Block block) {
   return calculateHash(
       block.index, block.previousHash, block.timestamp, block.data);
 }
 
+// Function to calculate a hash
 String calculateHash(
     int index, String previousHash, int timestamp, String data) {
   return sha256
@@ -178,12 +201,14 @@ String calculateHash(
       .toString();
 }
 
+// Function to add a block to the blockchain
 void addBlock(Block newBlock) {
   if (isValidNewBlock(newBlock, getLatestBlock())) {
     blockchain.add(newBlock);
   }
 }
 
+// Function to validate a new block
 bool isValidNewBlock(Block newBlock, Block previousBlock) {
   if (previousBlock.index + 1 != newBlock.index) {
     print('invalid index');
@@ -198,6 +223,7 @@ bool isValidNewBlock(Block newBlock, Block previousBlock) {
   return true;
 }
 
+// Function to connect to new peers
 void connectToPeers(List<String> newPeers) {
   for (var peerAddress in newPeers) {
     var url = Uri.parse(peerAddress);
@@ -208,6 +234,7 @@ void connectToPeers(List<String> newPeers) {
   }
 }
 
+// Function to handle the response from the blockchain
 void handleBlockchainResponse(Map<String, dynamic> message) {
   List<Block> receivedBlocks = (jsonDecode(message['data']) as List)
       .map((b) => Block.fromJson(b))
@@ -235,6 +262,7 @@ void handleBlockchainResponse(Map<String, dynamic> message) {
   }
 }
 
+// Function to replace the current blockchain with a new one
 void replaceChain(List<Block> newBlocks) {
   if (isValidChain(newBlocks) && newBlocks.length > blockchain.length) {
     print(
@@ -246,6 +274,7 @@ void replaceChain(List<Block> newBlocks) {
   }
 }
 
+// Function to validate a blockchain
 bool isValidChain(List<Block> blockchainToValidate) {
   if (jsonEncode(blockchainToValidate[0]) != jsonEncode(getGenesisBlock())) {
     return false;
@@ -261,24 +290,37 @@ bool isValidChain(List<Block> blockchainToValidate) {
   return true;
 }
 
+// Function to get the latest block in the blockchain
 Block getLatestBlock() => blockchain.last;
+
+// Function to create a message querying the length of the chain
 Map<String, dynamic> queryChainLengthMsg() =>
     {'type': MessageType.QUERY_LATEST.index};
+
+// Function to create a message querying all blocks
 Map<String, dynamic> queryAllMsg() => {'type': MessageType.QUERY_ALL.index};
+
+// Function to create a message with the entire blockchain
 Map<String, dynamic> responseChainMsg() => {
       'type': MessageType.RESPONSE_BLOCKCHAIN.index,
       'data': jsonEncode(blockchain)
     };
+
+// Function to create a message with the latest block
 Map<String, dynamic> responseLatestMsg() => {
       'type': MessageType.RESPONSE_BLOCKCHAIN.index,
       'data': jsonEncode([getLatestBlock()])
     };
 
+// Function to write a message to a socket
 void write(IOWebSocketChannel ws, Map<String, dynamic> message) =>
     ws.sink.add(jsonEncode(message));
+
+// Function to broadcast a message to all peers
 void broadcast(Map<String, dynamic> message) =>
     sockets.forEach((peer) => write(peer.socket, message));
 
+// Main function
 void main() {
   connectToPeers(initialPeers);
   initHttpServer();
